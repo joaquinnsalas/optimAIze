e9c938e346bf9e6cb40f3e270a21de1abc6ec8c3
jsalas2002 <jsalas2002@tamu.edu>
2025-06-07 02:19:45 -0500

    Remove large data folder from Git and ignore it

--- data/TAMU Classes/Spring 2024/ECEN360/Docker_SQLPython/sql_scripts/sqlscript.sql
+++
@@ -1,8 +0,0 @@
---SQL script example
-SELECT * FROM emails LIMIT 2;
-
-SELECT * FROM customers LIMIT 2;
-
-SELECT * FROM dealerships LIMIT 2;
-
-SELECT version()
--- data/TAMU Classes/Spring 2024/ECEN420/fb_design.m
+++
@@ -1,9 +0,0 @@
-function F = fb_design(poles)
-
-    A = [-1, 0; 0, -3];  % system dynamics matrix
-    B = [1/2; -1/2];          % input matrix
-    
-    % using the place function to compute the feedback matrix F
-    F = place(A, B, poles);
-    
-end
